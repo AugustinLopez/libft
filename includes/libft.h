ea9566aa1a87035bbb0307bb82187d95cc824f33
Augustin LOPEZ <aulopez@e1r4p14.42.fr>
2019-05-13 18:07:06 +0200

    More color on printf

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: aulopez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 08:40:39 by aulopez           #+#    #+#             */
-/*   Updated: 2019/04/11 15:39:15 by aulopez          ###   ########.fr       */
+/*   Updated: 2019/05/13 18:06:46 by aulopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,23 @@ typedef struct		s_list
 ** --- Color Macro -------------------------------------------------------------
 */
 
+# define FT_EOC			"\033[0m"
+# define FT_BOLD		"\033[1m"
+# define FT_DIM			"\033[2m"
+# define FT_ITALIC		"\033[3m"
+# define FT_UNDER		"\033[4m"
+# define FT_REV			"\033[7m"
+# define FT_HIDE		"\033[8m"
+# define FT_STRIKE		"\033[9m"
+# define FT_R_BOLD		"\033[21m"
+# define FT_R_DIM		"\033[22m"
+# define FT_R_ITALIC	"\033[23m"
+# define FT_R_UNDER		"\033[24m"
+# define FT_R_REV		"\033[27m"
+# define FT_R_HIDE		"\033[28m"
+# define FT_R_STRIKE	"\033[29m"
+
+# define FT_R_COLOR		"\033[39m"
 # define FT_BLACK		"\033[30m"
 # define FT_RED			"\033[31m"
 # define FT_GREEN		"\033[32m"
@@ -32,12 +49,33 @@ typedef struct		s_list
 # define FT_BLUE		"\033[34m"
 # define FT_PURPLE		"\033[35m"
 # define FT_CYAN		"\033[36m"
-# define FT_WHITE		"\033[37m"
-# define FT_EOC			"\033[0m"
-# define FT_BOLD		"\033[1m"
-# define FT_ITALIC		"\033[3m"
-# define FT_UNDER		"\033[4m"
-# define FT_STRIKE		"\033[9m"
+# define FT_GRAY		"\033[90m"
+# define FT_WHITE		"\033[97m"
+# define FT_LGRAY		"\033[37m"
+# define FT_LRED		"\033[91m"
+# define FT_LGREEN		"\033[92m"
+# define FT_LYELLOW		"\033[93m"
+# define FT_LBLUE		"\033[94m"
+# define FT_LPURPLE		"\033[95m"
+# define FT_LCYAN		"\033[96m"
+
+# define FT_R_BCOLOR	"\033[49m"
+# define FT_BBLACK		"\033[40m"
+# define FT_BRED		"\033[41m"
+# define FT_BGREEN		"\033[42m"
+# define FT_BYELLOW		"\033[43m"
+# define FT_BBLUE		"\033[44m"
+# define FT_BPURPLE		"\033[45m"
+# define FT_BCYAN		"\033[46m"
+# define FT_BGRAY		"\033[100m"
+# define FT_BWHITE		"\033[107m"
+# define FT_BLGRAY		"\033[47m"
+# define FT_BLRED		"\033[101m"
+# define FT_BLGREEN		"\033[102m"
+# define FT_BLYELLOW	"\033[103m"
+# define FT_BLBLUE		"\033[104m"
+# define FT_BLPURPLE	"\033[105m"
+# define FT_BLCYAN		"\033[106m"
 
 /*
 ** --- Print/Put Functions -----------------------------------------------------
