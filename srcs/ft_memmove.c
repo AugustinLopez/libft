fe7895a6d3e4393248cc5a10bc06c66ba5d3d211
Augustin LOPEZ <augustin@augustins-macbook-air.home>
2019-07-04 16:34:19 +0200

    updated memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -3,27 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aulopez <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aulopez <aulopez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 11:33:47 by aulopez           #+#    #+#             */
-/*   Updated: 2018/11/20 10:49:11 by aulopez          ###   ########.fr       */
+/*   Updated: 2019/07/04 16:30:16 by aulopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
+
+static inline void	longword(char **cdst, char **csrc, size_t *n)
+{
+	long long	*lldst;
+	long long	*llsrc;
+
+	while (((long long)*cdst & 0x7))
+	{
+		*((*cdst)--) = *((*csrc)--);
+		--(*n);
+	}
+	**cdst = **csrc;
+	lldst = (long long *)*cdst - 1;
+	llsrc = (long long *)*csrc - 1;
+	while (*n >= 8)
+	{
+		*lldst-- = *llsrc--;
+		*n -= 8;
+	}
+	if (*n)
+	{
+		*cdst = (char *)lldst - 1;
+		*csrc = (char *)llsrc - 1;
+	}
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*cdst;
-	char	*csrc;
+	char		*cdst;
+	char		*csrc;
 
-	cdst = (char*)dst;
-	csrc = (char*)src;
-	if (cdst < csrc)
-		while (len--)
-			*(cdst++) = *(csrc++);
-	else if (cdst > csrc)
-		while (len--)
-			cdst[len] = csrc[len];
+	if (dst <= src)
+		return (ft_memcpy(dst, src, len));
+	if (!len || (!dst && !src))
+		return (dst);
+	cdst = (char *)dst + len - 1;
+	csrc = (char *)src + len - 1;
+	if ((len >= 15) && (((long long)dst & 0x7) == ((long long)src & 0x7)))
+		longword(&cdst, &csrc, &len);
+	while (len--)
+		*cdst-- = *csrc--;
 	return (dst);
 }
